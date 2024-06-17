48fc09a550070aab5f9059fbd69554d789e91a1b
seeded-pro-team-owner <seeded-pro-team-owner@example.com>
2024-06-17 18:47:22 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjM0/spectral.js?branch_slug=master&token=4443bbe6-8cc6-4567-839f-3ee795fd4d99";
+export default { extends: ruleset };
